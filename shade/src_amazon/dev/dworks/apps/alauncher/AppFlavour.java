d28f3f794d949a505966737a62b62b9709372826
Hari Krishna Dulipudi <heart.break.kid.b4u@gmail.com>
2021-03-15 23:19:36 +1100

    add amazon iap

---
+++ shade/src_amazon/dev/dworks/apps/alauncher/AppFlavour.java
@@ -0,0 +1,223 @@
+package dev.dworks.apps.alauncher;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.util.ArrayMap;
+import android.widget.Toast;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.amazon.device.iap.model.Product;
+import com.amazon.device.iap.model.PurchaseResponse;
+import com.amazon.device.iap.model.Receipt;
+import com.android.launcher3.BuildConfig;
+import com.android.launcher3.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import amirz.App;
+import amirz.helpers.Settings;
+import dev.dworks.apps.alauncher.misc.BillingHelper;
+import needle.Needle;
+import needle.UiRelatedTask;
+
+/**
+ * Created by HaKr on 16/05/17.
+ */
+
+public abstract class AppFlavour extends Application implements BillingHelper.BillingListener {
+	private static final String PURCHASE_PRODUCT_ID = "purchase_product_id";
+	public static final String PURCH_ID = BuildConfig.APPLICATION_ID + ".purch";
+	public static final String PURCHASED = "purchased";
+	private static final int IAP_ID_CODE = 1;
+	public static final String BILLING_ACTION = "BillingInitialized";
+
+	private String currentProductId = "";
+	private BillingHelper billingHelper;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+
+	public static boolean isPurchased() {
+		return Settings.isProVersion() || PreferenceUtils.getBooleanPrefs(PURCHASED)
+				|| BuildConfig.DEBUG;
+	}
+
+	public void initializeBilling(Activity activity) {
+		List<String> skuList = new ArrayList<>();
+		skuList.add(getPurchaseId());
+		billingHelper = new BillingHelper(this, this);
+		billingHelper.setSkuInAppList(skuList);
+		billingHelper.setCurrentActivity(activity);
+		billingHelper.initialize();
+	}
+
+	public void releaseBilling() {
+		if(null == billingHelper){
+			return;
+		}
+		billingHelper.endConnection();
+	}
+
+	public void loadPurchaseItems(Activity activity){
+		Needle.onBackgroundThread().execute(new UiRelatedTask<Boolean>(){
+			@Override
+			protected Boolean doWork() {
+				billingHelper.getOwnedItems();
+				return true;
+			}
+
+			@Override
+			protected void thenDoUiRelatedWork(Boolean aBoolean) {
+				if (App.isPurchased()) {
+					Settings.showSnackBar(activity, R.string.restored_previous_purchase_please_restart);
+					finishDelayed(activity);
+				} else {
+					Settings.showSnackBar(activity, R.string.could_not_restore_purchase);
+				}
+			}
+		});
+	}
+
+	public String getPurchasePrice(String productId){
+		return billingHelper.getSkuPrice(productId);
+	}
+
+	@Override
+	public void onSkuListResponse(ArrayMap<String, Product> skuDetailsMap) {
+		LocalBroadcastManager.getInstance(getApplicationContext())
+				.sendBroadcast(new Intent(BILLING_ACTION));
+	}
+
+	@Override
+	public void onPurchaseHistoryResponse(List<Receipt> purchasedList) {
+		boolean isPurchased = false;
+		String currentId = getPurchasedProductId();
+		for (Receipt purchase: purchasedList) {
+			if(currentId.equals(purchase.getSku())){
+				isPurchased  = true;
+				break;
+			}
+		}
+		PreferenceUtils.set(PURCHASED, isPurchased);
+		LocalBroadcastManager.getInstance(getApplicationContext())
+				.sendBroadcast(new Intent(BILLING_ACTION));
+	}
+
+	@Override
+	public void onPurchaseCompleted(Activity activity, Receipt purchaseItem) {
+		Settings.showSnackBar(activity, R.string.thank_you);
+		PreferenceUtils.set(PURCHASE_PRODUCT_ID, purchaseItem.getSku());
+		PreferenceUtils.set(PURCHASED, true);
+		finishDelayed(activity);
+	}
+
+	@Override
+	public void onPurchaseError(Activity activity, PurchaseResponse.RequestStatus errorCode) {
+		String message = "";
+		String action = "";
+		switch (errorCode){
+			case ALREADY_PURCHASED:
+				if(!TextUtils.isEmpty(currentProductId)) {
+					PreferenceUtils.set(PURCHASE_PRODUCT_ID, currentProductId);
+				}
+				message = "Purchase restored";
+				break;
+			case FAILED:
+				message = "Payment flow cancelled";
+				break;
+			case INVALID_SKU:
+				message = "Billing not available. Contact Developer";
+				action = "Contact";
+				break;
+			case NOT_SUPPORTED:
+				message = "In App Purchase not available. Buy Pro App directly.";
+				action = "Buy";
+				break;
+			default:
+				message = "Something went wrong! error code="+ errorCode+". Contact Developer";
+				action = "Contact";
+				break;
+		}
+		if(null != activity) {
+			if(TextUtils.isEmpty(action)) {
+				Settings.showSnackBar(activity, message);
+			} else {
+				String finalMessage = message;
+				Settings.showSnackBar(activity, message, action, new Runnable() {
+					@Override
+					public void run() {
+						billingErrorAction(activity, finalMessage, errorCode);
+					}
+				});
+			}
+		} else {
+			try {
+				Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+			} catch (Exception e){ }
+		}
+	}
+
+	private void billingErrorAction(Activity activity, String message, PurchaseResponse.RequestStatus errorCode){
+		switch (errorCode){
+			case NOT_SUPPORTED:
+				Settings.openProAppLink(activity);
+				break;
+			default:
+				String details = "Error:  \n";
+				details	+= message+"\n";
+				details += "Billing error code:"+errorCode;
+				Settings.sendError(activity, details);
+				break;
+		}
+	}
+
+	public static boolean isBillingSupported() {
+		return BillingHelper.isBillingAvailable(App.getInstance().getApplicationContext());
+	}
+
+	public static String getPurchaseId(){
+		return PURCH_ID+ ".pro" + IAP_ID_CODE;
+	}
+
+	public static String getPurchasedProductId(){
+		String productId = PreferenceUtils.getStringPrefs(PURCHASE_PRODUCT_ID);
+		return !TextUtils.isEmpty(productId) ? productId : getPurchaseId();
+	}
+
+	public static boolean isActivityAlive(Activity activity) {
+		return !(null == activity || activity.isDestroyed());
+	}
+
+	private static void finishDelayed(Activity activity){
+		if(!isActivityAlive(activity)){
+			return;
+		}
+		new Handler().postDelayed(new Runnable(){
+			@Override
+			public void run() {
+				activity.finish();
+			}
+		}, 4000);
+	}
+
+	public void purchase(Activity activity, String productId){
+		if(isBillingSupported()) {
+			currentProductId = productId;
+			billingHelper.launchBillingFLow(activity, productId);
+		} else {
+			Settings.showSnackBar(activity, "Billing not supported");
+		}
+	}
+
+	public void openPurchaseActivity(Context context){
+		context.startActivity(new Intent(context, PurchaseActivity.class));
+	}
+}
---
+++ shade/src_amazon/dev/dworks/apps/alauncher/misc/BillingHelper.java
@@ -0,0 +1,276 @@
+package dev.dworks.apps.alauncher.misc;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.ArrayMap;
+
+import com.amazon.device.iap.PurchasingListener;
+import com.amazon.device.iap.PurchasingService;
+import com.amazon.device.iap.model.FulfillmentResult;
+import com.amazon.device.iap.model.Product;
+import com.amazon.device.iap.model.ProductDataResponse;
+import com.amazon.device.iap.model.ProductType;
+import com.amazon.device.iap.model.PurchaseResponse;
+import com.amazon.device.iap.model.PurchaseUpdatesResponse;
+import com.amazon.device.iap.model.Receipt;
+import com.amazon.device.iap.model.UserDataResponse;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static com.amazon.device.iap.model.PurchaseResponse.RequestStatus.ALREADY_PURCHASED;
+
+
+public class BillingHelper implements PurchasingListener {
+
+    private Context context;
+    private BillingListener mBillingListener;
+    private List<String> skuInAppList = new ArrayList<>();
+    private List<String> skuSubList = new ArrayList<>();
+    private ArrayMap<String, Product> skuDetailsMap = new ArrayMap<String, Product>();
+    private ArrayMap<String, Receipt> purchaseDetailsMap = new ArrayMap<String, Receipt>();
+    private boolean mSubscriptionSupported = false;
+    private Activity mCurrentActivity;
+
+    /**
+     * To instantiate the object
+     *  @param context           It will be used to get an application context to bind to the in-app billing service.
+     * @param listener          Your listener to get the response for your query.
+     */
+    public BillingHelper(Context context, BillingListener listener) {
+        this.context = context;
+        this.mBillingListener = listener;
+    }
+
+    public void setSkuInAppList(List<String> skuInAppList) {
+        this.skuInAppList = skuInAppList;
+    }
+
+    public void setSkuSubList(List<String> skuSubList) {
+        this.skuSubList = skuSubList;
+        this.mSubscriptionSupported = true;
+    }
+
+    public void initialize(){
+        PurchasingService.registerListener(this.context, this);
+        getOwnedItems();
+        getSKUDetails();
+    }
+
+    public void setCurrentActivity(Activity activity) {
+        this.mCurrentActivity = activity;
+    }
+
+    public ArrayMap<String, Receipt> getPurchaseDetailsMap() {
+        return purchaseDetailsMap;
+    }
+
+    public void getOwnedItems() {
+        getPurchasedItems();
+    }
+    /**
+     * Get purchases details for all the items bought within your app.
+     */
+    public void getPurchasedItems() {
+        PurchasingService.getPurchaseUpdates(true);
+    }
+
+    /**
+     * Get purchases details for all the items bought within your app.
+     */
+    public void getSubscribedItems() {
+        PurchasingService.getPurchaseUpdates(true);
+    }
+
+    /**x
+     * Perform a network query to get SKU details and return the result asynchronously.
+     */
+    public void getSKUDetails() {
+        getSKUInAppDetails();
+        getSKUSubDetails();
+    }
+
+    /**
+     * Perform a network query to get In App SKU details and return the result asynchronously.
+     */
+    public void getSKUInAppDetails() {
+        if(null == skuInAppList || skuInAppList.isEmpty()){
+            return;
+        }
+        Set<String> keys = new HashSet<>();
+        keys.addAll(this.skuInAppList);
+        PurchasingService.getProductData(keys);
+    }
+
+    /**
+     * Perform a network query to get Sub SKU details and return the result asynchronously.
+     */
+    public void getSKUSubDetails() {
+        if(null == skuSubList || skuSubList.isEmpty()){
+            return;
+        }
+        Set<String> keys = new HashSet<>();
+        keys.addAll(this.skuSubList);
+        PurchasingService.getProductData(keys);
+    }
+
+    /**
+     * Initiate the billing flow for an in-app purchase or subscription.
+     *
+     * @param productId product Id of the product to be purchased
+     *                   Developer console.
+     */
+    public void launchBillingFLow(Activity activity, final String productId) {
+        synchronized (skuDetailsMap) {
+            Product skuDetails = skuDetailsMap.get(productId);
+            if(null == skuDetails){
+                return;
+            }
+            mCurrentActivity = activity;
+            PurchasingService.purchase(productId);
+        }
+    }
+
+    //This is for Non-Consumable product
+    public void acknowledgePurchase(Receipt purchase) {
+        if (isSignatureValid(purchase)) {
+            PurchasingService.notifyFulfillment(purchase.getReceiptId(), FulfillmentResult.FULFILLED);
+            if (mBillingListener != null) {
+                mBillingListener.onPurchaseCompleted(mCurrentActivity, purchase);
+            }
+        }
+    }
+
+    //This is for Consumable product
+    public void consumePurchase(Receipt purchase) {
+        if (isSignatureValid(purchase)) {
+            PurchasingService.notifyFulfillment(purchase.getReceiptId(), FulfillmentResult.FULFILLED);
+            if (mBillingListener != null) {
+                mBillingListener.onPurchaseCompleted(mCurrentActivity, purchase);
+            }
+        }
+    }
+
+    public String getSkuPrice(String productId){
+        String priceValue = "";
+        synchronized (skuDetailsMap) {
+            Product sku = skuDetailsMap.get(productId);
+            if (null != sku) {
+                priceValue = sku.getPrice();
+            }
+        }
+        return priceValue;
+    }
+
+    public Product getSkuDetails(String productId){
+        synchronized (skuDetailsMap) {
+            return skuDetailsMap.get(productId);
+        }
+    }
+
+    public Receipt getPurchaseDetails(String productId){
+        synchronized (purchaseDetailsMap) {
+            return purchaseDetailsMap.get(productId);
+        }
+    }
+
+    private boolean isSignatureValid(Receipt purchase) {
+        return true;
+    }
+
+    public static boolean isBillingAvailable(Context context){
+        return true;
+    }
+
+    /**
+     * Call this method once you are done with this BillingClient reference.
+     */
+    public void endConnection() {
+
+    }
+
+    @Override
+    public void onUserDataResponse(UserDataResponse userDataResponse) {
+
+    }
+
+    @Override
+    public void onProductDataResponse(ProductDataResponse response) {
+        final ProductDataResponse.RequestStatus status = response.getRequestStatus();
+
+        if (status == ProductDataResponse.RequestStatus.SUCCESSFUL) {
+            final Set<String> unavailableSkus = response.getUnavailableSkus();
+            Map<String, Product> productData = response.getProductData();
+            synchronized (skuDetailsMap) {
+                for (Map.Entry<String, Product> entry : productData.entrySet()) {
+                    Product product = productData.get(entry.getKey());
+                    skuDetailsMap.put(product.getSku(), product);
+                }
+                if (mBillingListener != null) {
+                    mBillingListener.onSkuListResponse(skuDetailsMap);
+                }
+            }
+        } else if (mBillingListener != null) {
+            mBillingListener.onPurchaseError(mCurrentActivity, PurchaseResponse.RequestStatus.NOT_SUPPORTED);
+        }
+    }
+
+    @Override
+    public void onPurchaseResponse(PurchaseResponse response) {
+        final PurchaseResponse.RequestStatus status = response.getRequestStatus();
+        final Receipt receipt;
+        if (status == PurchaseResponse.RequestStatus.SUCCESSFUL) {
+            //here when purchase completed
+            receipt = response.getReceipt();
+            if (receipt.getProductType() == ProductType.SUBSCRIPTION) {
+                consumePurchase(receipt);
+            } else {
+                acknowledgePurchase(receipt);
+            }
+        } else if (status == ALREADY_PURCHASED) {
+            getOwnedItems();
+        } else if (mBillingListener != null) {
+            mBillingListener.onPurchaseError(mCurrentActivity, status);
+        }
+    }
+
+    @Override
+    public void onPurchaseUpdatesResponse(PurchaseUpdatesResponse response) {
+        if (response == null) {
+            return;
+        }
+        final PurchaseUpdatesResponse.RequestStatus status = response.getRequestStatus();
+        if (status == PurchaseUpdatesResponse.RequestStatus.SUCCESSFUL) {
+            Receipt[] receiptsList = response.getReceipts().toArray(new Receipt[0]);
+            if(receiptsList.length == 0){
+                return;
+            }
+            synchronized (purchaseDetailsMap) {
+                for (Receipt receipt : receiptsList) {
+                    if (receipt != null && !receipt.isCanceled()) {
+                        purchaseDetailsMap.put(receipt.getSku(), receipt);
+                    }
+                }
+                if (mBillingListener != null) {
+                    mBillingListener.onPurchaseHistoryResponse(Arrays.asList(receiptsList));
+                }
+            }
+        } else if (mBillingListener != null) {
+            mBillingListener.onPurchaseError(mCurrentActivity, PurchaseResponse.RequestStatus.NOT_SUPPORTED);
+        }
+    }
+
+    /**
+     * Listener interface for handling the various responses of the Purchase helper util
+     */
+    public interface BillingListener {
+        void onSkuListResponse(ArrayMap<String, Product> skuDetailsMap);
+        void onPurchaseHistoryResponse(List<Receipt> purchasedList);
+        void onPurchaseCompleted(Activity activity, Receipt purchaseItem);
+        void onPurchaseError(Activity activity, PurchaseResponse.RequestStatus errorCode);
+    }
+}
